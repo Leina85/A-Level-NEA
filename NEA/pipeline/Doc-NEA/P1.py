e0ef52e26178e4380667d2be674297dc526a71fd
Leina85 <leinajen@icloud.com>
2025-11-08 13:54:11 +0000

    imputs and generating pore found chance

--- NEA/pipeline/Doc-NEA/P1.py
+++ NEA/pipeline/Doc-NEA/P1.py
@@ -3,7 +3,10 @@ import numpy as np
 # Specify parameters
 mean = 7472.521
 std_dev = 3024.544
-n = 1
+n = 100
+
+BPS = 450
+DNA_FOUND_CHANCE = 0.5632678551
 
 # Generate random numbers
 data = np.random.normal(mean, std_dev, n).astype(int)
@@ -11,3 +14,9 @@ data = np.clip(data, 1, None)
 
 # Display the generated number
 print(data)
+
+runtime = input('Enter runtime: ')
+avg_molecule_length = input('Enter Average Molecule Length: ')
+
+result = np.random.random() < DNA_FOUND_CHANCE
+print('here', result)
